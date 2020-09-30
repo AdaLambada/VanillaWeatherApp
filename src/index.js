4e91d1ac242acec957689c85d696d93c019ea30c
AdaLambada <69824448+AdaLambada@users.noreply.github.com>
2020-09-30 20:39:13 +0200

    added current day time
    left

---
+++ src/index.js
@@ -0,0 +1,21 @@
+let now = new Date();
+let dateToday = document.querySelector("#currentDayTime");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currentDay = days[now.getDay()];
+let currentHour = now.getHours();
+if (currentHour < 10) {
+  currentHour = `0${currentHour}`;
+}
+let currentMinutes = now.getMinutes();
+if (currentMinutes < 10) {
+  currentMinutes = `0${currentMinutes}`;
+}
+dateToday.innerHTML = `${currentDay} ${currentHour}:${currentMinutes}`;
