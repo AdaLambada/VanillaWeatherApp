40af1b0865df2f3b09e919ad63cd36b4718c3f5a
AdaLambada <69824448+AdaLambada@users.noreply.github.com>
2020-09-30 20:41:34 +0200

    added function showWeather

--- src/index.js
+++ src/index.js
@@ -19,3 +19,18 @@ if (currentMinutes < 10) {
   currentMinutes = `0${currentMinutes}`;
 }
 dateToday.innerHTML = `${currentDay} ${currentHour}:${currentMinutes}`;
+
+function showWeather(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#current-temp");
+  currentTemperature.innerHTML = `${temperature}°C`;
+  let currentCity = document.querySelector("h2");
+  currentCity.innerHTML = response.data.name;
+  document.querySelector("#current-humidity").innerHTML =
+    response.data.main.humidity;
+  document.querySelector("#current-wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+}
